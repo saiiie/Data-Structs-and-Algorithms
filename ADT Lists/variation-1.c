0eeb219125f0fffde7f38a82f2861be45c39d2a1
Sybel Yadeen <aquillasybelyadeen123@gmail.com>
2025-08-21 18:49:30 +0800

    Add insertSorted function and update main logic
    Introduced the insertSorted function to insert elements in sorted order into the list. Updated main to demonstrate sorted insertions and added an extra deletePos call. The displayList function is called after each operation to show the current state of the list.

--- ADT Lists/variation-1.c
+++ ADT Lists/variation-1.c
@@ -32,6 +32,7 @@ int main(){
 
     L = deletePos(L, 5);
     L = deletePos(L, 7);
+    L = deletePos(L, 7);
     displayList(L);
 
     for(i=0; i<size/2; i++){
@@ -42,6 +43,12 @@ int main(){
             printf("%d not in list!\n", i);
         }
     }
+
+    L = insertSorted(L, 3);
+    L = insertSorted(L, 6);
+    L = insertSorted(L, 9);
+    L = insertSorted(L, 10);
+    displayList(L);
 
     return 0;
 }
@@ -99,7 +106,19 @@ int locate(List L, int data){
 }
 
 List insertSorted(List L, int data){
+    if (L.count == size){
+        printf("List is full!\n");
+        return L;
+    }
+
+    int pos = 0;
+    while (pos < L.count && L.elem[pos] < data) pos++;
+    for (int j = L.count - 1; j >= pos; j--) L.elem[j+1] = L.elem[j];
     
+    L.elem[pos] = data;
+    L.count++;
+    printf("Inserted %d at position %d.\n", data, pos);
+    return L;
 }
 
 void displayList(List L){
