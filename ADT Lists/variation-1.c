aace7c9887674ffc2322890fa76ebb17475b2ae6
Sybel Yadeen <aquillasybelyadeen123@gmail.com>
2025-08-21 14:38:45 +0800

    Move variation-1 list ADT files to ADT Lists folder
    Relocated variation-1.c and variation-1.exe to the ADT Lists directory, updating the implementation and test data. Added .vscode/settings.json for compiler configuration. Removed the original files from the root directory.

---
+++ ADT Lists/variation-1.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define size 10
+
+typedef struct{
+    int elem[size];
+    int count;
+} List;
+
+List initialize(List);
+List insertPos(List, int, int);
+List deletePos(List, int);
+int locate(List, int);
+List insertSorted(List, int);
+void displayList(List);
+
+int main(){
+    int pos, i;
+    List L = initialize(L);
+
+    L = insertPos(L, 2, 0);
+    L = insertPos(L, 1, 0);
+    L = insertPos(L, 4, 2);
+    L = insertPos(L, 5, 3);
+    L = insertPos(L, 7, 4);
+    L = insertPos(L, 7, 4);
+    L = insertPos(L, 8, 6);
+    L = insertPos(L, 9, 7);
+    L = insertPos(L, 8, 7);
+    displayList(L);
+
+    L = deletePos(L, 5);
+    L = deletePos(L, 7);
+    displayList(L);
+
+    for(i=0; i<size/2; i++){
+        pos = locate(L, i);
+        if (pos != -1){
+            printf("%d located at position %d.\n", i, pos);
+        } else{
+            printf("%d not in list!\n", i);
+        }
+    }
+
+    return 0;
+}
+
+List initialize(List L){
+    L.count = 0;
+    return L;
+}
+
+List insertPos(List L, int data, int pos){
+    if (L.count == size){
+        printf("List is full!\n");
+        return L;
+    }
+
+    if (pos > L.count || pos < 0){
+        printf("Cannot insert! Valid positions 0-%d only.\n", L.count);
+        return L;
+    }
+
+    int i;
+    for (i = L.count-1; i >= pos; i--){
+        L.elem[i+1] = L.elem[i];
+    }
+        
+    L.elem[pos] = data;
+    L.count++;
+    printf("Data %d inserted at position %d.\n", data, pos);
+    return L;
+}
+
+List deletePos(List L, int pos){
+    if (pos > L.count || pos < 0){
+        printf("Cannot delete! Valid positions 0-%d only.\n", L.count);
+        return L;
+    }
+
+    for (int i=pos; i<L.count; i++){
+        L.elem[i] = L.elem[i+1];
+    }
+
+    L.count--;
+    printf("Data deleted at position %d.\n", pos);
+    return L;
+}
+
+int locate(List L, int data){
+    for (int i=0; i<L.count; i++){
+        if (L.elem[i] == data){
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+List insertSorted(List L, int data){
+    
+}
+
+void displayList(List L){
+    printf("\nUpdated List: ");
+    for(int i=0; i<L.count;i++){
+        printf("%d ", L.elem[i]);
+    }
+    printf("\n\n");
+}
